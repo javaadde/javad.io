aad58dc30826958780f40d09f77e8a2b4712c6ce
kuttappi <iamkuttappiii@gmail.com>
2024-10-10 12:43:46 +0530

    added navigation bar with script thats super cool

---
+++ script.js
@@ -0,0 +1,11 @@
+function OpenNev(){
+    document.getElementById("nav-mob").style.display = "flex"
+    document.getElementById("m-i").style.display = "none"
+    document.getElementById("m-ci").style.display = "flex"
+}
+
+function CloseNev(){
+    document.getElementById("nav-mob").style.display = "none"
+    document.getElementById("m-ci").style.display = "none"
+    document.getElementById("m-i").style.display = "flex"
+}
